0c6186fab7e05c64cae4cd315b2994398f719508
rohini <rohini@DESKTOP-OI9JR7I>
2025-10-14 14:11:30 +0530

    interfas

---
+++ tnsif/src/interfas/FunctionalDemo.java
@@ -0,0 +1,18 @@
+package interfas;
+
+
+public class FunctionalDemo {
+
+	public static void main(String[] args) {
+	
+		//Lambda Expression
+		GreetingDemo gd = () -> 
+		System.out.println("Hello Everyone, Welcome to the Java Session");
+		
+		gd.greet();
+		
+		
+
+	}
+
+}
---
+++ tnsif/src/interfas/GreetingDemo.java
@@ -0,0 +1,8 @@
+package interfas;
+
+@FunctionalInterface
+public interface GreetingDemo {
+	 void greet(); //used for declaration
+	 //void greet1();
+
+}
